acc7aa3efddbfdae4be526760bb26e78cf7d9f56
Chiyoung Won <wcyoung98@gmail.com>
2020-07-12 12:34:23 +0900

    Spring Boot init

---
+++ src/test/java/wcyoung/spring/boot/SpringBootApiApplicationTests.java
@@ -0,0 +1,14 @@
+package wcyoung.spring.boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+
+    }
+
+}
